12b39097e09f9babb2888a60f29fd6797e074d4e
Ben Joe Mathews <joemathews4development@gmail.com>
2026-01-14 16:36:12 +0100

    Solved JS datatypes activity

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ const s5 = "and";
 
 // Concatenate the string variables into one new string
 
+const tongueTwister = `${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}`
 
 // Print out the concatenated string
 
-
+console.log(tongueTwister)
 
 
 /*******************************************
@@ -22,10 +23,14 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
+const part1Length = part1.length
+const part2Length = part2.length
+const part1CamelTailed = part1.slice(0, part1Length - 1) + part1[part1Length - 1].toUpperCase()
+const part2CamelTailed = part2.slice(0, part2Length - 1) + part2[part2Length - 1].toUpperCase()
+const result = part1CamelTailed + part2CamelTailed
 
 // Print the cameLtaiL-formatted string
-
+console.log(result)
 
 
 
@@ -35,11 +40,11 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+const tipAmount = billTotal * 0.15
 
 // Print out the tipAmount
 
-
+console.log(tipAmount)
 
 
 /*******************************************
@@ -47,11 +52,12 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+const randomDecimal = Math.random() * 10
+const randomNumber = Math.round(randomDecimal)
 
 // Print the generated random number
 
-
+console.log(randomNumber)
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -74,3 +80,13 @@ const expression5 = !a || !b;
 const expression6 = !(a || b);
 
 const expression7 = a && a;
+/*
+expression1 = false
+expression2 = true
+expression3 = false
+expression4 = true
+expression5 = true
+expression6 = false
+expression7 = true
+*/
+console.log(expression1, expression2, expression3, expression4, expression5, expression6, expression7)
